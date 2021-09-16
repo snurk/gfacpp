615cfae8d90e29116d895b41dd73cf35e5000edc
Sergey Nurk <sergeynurk@gmail.com>
2021-09-16 00:08:24 -0400

    support rename_all option; improved verbosity

--- src/tooling.hpp
+++ src/tooling.hpp
@@ -39,16 +39,21 @@ inline
 clipp::group BaseCfg(cmd_cfg_base &cfg) {
     using namespace clipp;
 
-    return ( cfg.graph_in << value("input file in GFA (ending with .gfa)"),
+    auto grp = ( cfg.graph_in << value("input file in GFA (ending with .gfa)"),
             cfg.graph_out << value("output file"),
             (option("--coverage") & value("file", cfg.coverage)) % "file with coverage information",
             option("--compact").set(cfg.compact) % "compact the graph after cleaning (default: false)",
             (option("--id-mapping") & value("file", cfg.id_mapping)) % "file with compacted segment id mapping",
             (option("--prefix") & value("vale", cfg.compacted_prefix)) % "prefix used to form compacted segment names (default: m_, use _ for empty)",
             option("--drop-sequence").set(cfg.drop_sequence) % "flag to drop sequences even if present in original file (default: false)",
-            option("--rename-all").set(cfg.rename_all) % "flag to rename all segments (default: false)",
+            option("--rename-all").set(cfg.rename_all) % "flag to rename all segments. Enforces compaction (default: false)",
             (option("--dbg-k") & integer("value", cfg.dbg_k)) % "DBG k-mer length to use in coverage transformation (default: 0 -- disabled)"
     ) % "common settings";
+
+    if (cfg.compact) {
+        INFO("Graph will be re-compacted in the end. Prefix: " << cfg.compacted_prefix);
+    }
+    return grp;
 }
 
 void OutputGraph(gfa::Graph &g,
@@ -65,10 +70,10 @@ void OutputGraph(gfa::Graph &g,
 
     assert(g.CheckNoDeadLinks());
 
-    if (ndel > 0 && cfg.compact) {
+    if (cfg.rename_all || (ndel > 0 && cfg.compact)) {
         gfa::Compactifier compactifier(g, cfg.compacted_prefix, segment_cov_ptr, cfg.dbg_k);
         std::cout << "Writing compacted graph to " << cfg.graph_out << std::endl;
-        compactifier.Compact(cfg.graph_out, cfg.id_mapping, cfg.drop_sequence);
+        compactifier.Compact(cfg.graph_out, cfg.id_mapping, cfg.drop_sequence, cfg.rename_all);
     } else {
         std::cout << "Writing output to " << cfg.graph_out << std::endl;
         g.write(cfg.graph_out, cfg.drop_sequence);
